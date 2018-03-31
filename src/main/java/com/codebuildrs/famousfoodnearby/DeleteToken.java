f7fe69b5723dbafec9a7aab091f919440bbd45c0
kagowtham <ka.gow1996@gmail.com>
2018-03-31 11:18:34 +0530

    added delete token

--- src/main/java/com/codebuildrs/famousfoodnearby/DeleteToken.java
+++ src/main/java/com/codebuildrs/famousfoodnearby/DeleteToken.java
@@ -9,33 +9,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.bson.Document;
 
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
-/**
- * Servlet implementation class Token
- */
-public class Token extends HttpServlet {
+
+public class DeleteToken extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+       
+   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String name =request.getParameter("name");
 		String id=request.getParameter("id");
-		System.out.println("name :"+name);
-		System.out.println("id :"+id);
-	
 		MongoCollection<Document> collection=Database.database.getCollection("tokens");
-		
-		if(collection.count(new Document("name",name))>0) {
-			response.getWriter().append("available");
-		}else {
-		    collection.insertOne(new Document("name",name).append("id", id));
-		    response.getWriter().append("true");
-		}
+		collection.deleteMany(new Document("id",id));
+	    response.getWriter().append("true");
+
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		doGet(request, response);
 	}
 
