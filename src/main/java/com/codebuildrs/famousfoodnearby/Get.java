b3baaae35b6435dc0762acefd5094d5416cd29bd
kagowtham <ka.gow1996@gmail.com>
2018-04-21 09:47:15 +0530

    bug fixed

--- src/main/java/com/codebuildrs/famousfoodnearby/Get.java
+++ src/main/java/com/codebuildrs/famousfoodnearby/Get.java
@@ -7,10 +7,12 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.Iterator;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 import org.bson.Document;
 import org.json.JSONArray;
@@ -18,6 +20,8 @@ import org.json.JSONObject;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+
+
 
 
 public class Get extends HttpServlet {
@@ -34,52 +38,55 @@ public class Get extends HttpServlet {
 		FindIterable<Document> iterable = collection.find(
 		        new Document("lat", new Document("$gte",lat-0.3).append("$lte", lat+0.3)).append("log", new Document("$gte",log-0.3).append("$lte", log+0.3)));
 		Iterator<Document> iterator=iterable.iterator();
-		JSONArray json=new JSONArray();
+		final JSONArray json=new JSONArray();
 		while(iterator.hasNext()) {
-			Document d=iterator.next();
-			 String mainUrl="http://maps.googleapis.com/maps/api/directions/json?";
+			             Document d=iterator.next();
+                         String res = "";
+			             try
+			             {
+			                 //prepare connection
+			                 myURL = new URL("https://maps.googleapis.com/maps/api/directions/json?origin="+lat+","+log+"&destination="+d.get("lat")+","+d.get("log")+"&key=AIzaSyCUPHvDyNWRiOtEWQaO5UEmGksFZGmTlsw");
+			                 myURLConnection = myURL.openConnection();
+			                 myURLConnection.connect();
+			                 reader= new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
 
+			                 //reading response
+			                 String r="";
+			                 while ((r = reader.readLine()) != null) {
+			                    // System.out.println(r);
+			                     res+=r;
+			                 }
+                                
+			                 //finally close connection
+			                 reader.close();
 
-	            StringBuilder sbPostData= new StringBuilder(mainUrl);
-	            sbPostData.append("origin="+lat+","+log);
-	            sbPostData.append("&destination="+d.get("lat")+","+d.get("log"));
-	            sbPostData.append("&sensor=false&units=metric");
-	            mainUrl = sbPostData.toString();
-	            try
-	            {
-	                //prepare connection
-	                myURL = new URL(mainUrl);
-	                myURLConnection = myURL.openConnection();
-	                myURLConnection.connect();
-	                reader= new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+			                
+			             }
+			             catch (IOException e)
+			             {
+			                 e.printStackTrace();
+			                 
+			             }
+						JSONObject json1 = new JSONObject(res);
+		                JSONArray routeArray = json1.getJSONArray("routes");
+		                JSONObject routes = routeArray.getJSONObject(0);
 
-	                //reading response
-	                String response1;
-	                while ((response1 = reader.readLine()) != null)
-	                    //print response
-	                System.out.println("response from "+mainUrl+"\n"+response1);
-	                JSONObject json1 = new JSONObject(response1);
-	                JSONArray routeArray = json1.getJSONArray("routes");
-	                JSONObject routes = routeArray.getJSONObject(0);
+		                JSONArray newTempARr = routes.getJSONArray("legs");
+		                JSONObject newDisTimeOb = newTempARr.getJSONObject(0);
 
-	                JSONArray newTempARr = routes.getJSONArray("legs");
-	                JSONObject newDisTimeOb = newTempARr.getJSONObject(0);
+		                JSONObject distOb = newDisTimeOb.getJSONObject("distance");
+		                JSONObject timeOb = newDisTimeOb.getJSONObject("duration");
 
-	                JSONObject distOb = newDisTimeOb.getJSONObject("distance");
-	                JSONObject timeOb = newDisTimeOb.getJSONObject("duration");
-
-	               String Distance =distOb.getString("text");
-	               String Time=timeOb.getString("text");
-	               json.put(d.append("dis",Distance).append("time", Time));
-	                //finally close connection
-	                reader.close();
+		               String Distance =String.valueOf(distOb.get("value"));
+		               String Time=timeOb.getString("text");
+		               json.put(d.append("dis",Distance).append("time", Time));
+					
+			             
+	                
+	               
 	            }
-	            catch (IOException e)
-	            {
-	                e.printStackTrace();
-	            }
-			
-		}
+		
+		
 		response.getWriter().append(json.toString());
 	}
 
